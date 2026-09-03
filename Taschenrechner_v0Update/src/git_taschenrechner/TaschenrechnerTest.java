317cdf215ebed564d3103f15c1eb18113e53561c
Benjamin Lehmberg <user@192.168.34.93>
2026-09-03 10:36:07 +0200

    Implementierrung von der Taschenrechner und TaschenrechnerTest Klasse. Aufgaeb A 2.5

--- Taschenrechner_v0Update/src/git_taschenrechner/TaschenrechnerTest.java
+++ Taschenrechner_v0Update/src/git_taschenrechner/TaschenrechnerTest.java
@@ -1,11 +1,11 @@
 package git_taschenrechner;
 
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class TaschenrechnerTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		Scanner myScanner = new Scanner(System.in);
 		Taschenrechner ts = new Taschenrechner();
@@ -19,26 +19,45 @@ public class TaschenrechnerTest {
 		System.out.println("| Options:                 |");
 		System.out.println("|        1. Addieren       |");
 		System.out.println("|        2. Subtrahieren   |");
-		System.out.println("|        3. Dividieren     |");
-		System.out.println("|        4. Exit           |");
+		System.out.println("|        3. Multiplikation |");
+		System.out.println("|        4. Dividieren     |");
+		System.out.println("|        5. Exit           |");
 		System.out.println("============================");
 		System.out.print(" Select option: ");
 		swValue = myScanner.next().charAt(0);
 
-		// Switch construct
-		switch (swValue) {
-		case '1':
-			System.out.println("4 + 5 = " + ts.add(4, 5));
-			break;
-			
-			
-		  //  add your code here
-		  
-		  
-		default:
-			System.out.println("Invalid selection");
-			break; // This break is not really necessary
+		if(swValue == '1' || swValue == '2' ||swValue == '3' ||swValue == '4') {
+
+
+			System.out.println("Geben sie die ganz erste Zahl ein:");
+			int zahl1 = myScanner.nextInt();
+			System.out.println("Geben sie die ganz zweite Zahl ein:");
+			int zahl2 = myScanner.nextInt();
+
+			// Switch construct
+			switch (swValue) {
+			case '1':
+				System.out.println(zahl1 + " + " + zahl2 + " = " + ts.add(zahl1, zahl2));
+				break;
+
+			case '2':
+				System.out.println(zahl1 + " - " + zahl2 + " = " + ts.sub(zahl1, zahl2));
+				break;
+
+			case '3':
+				System.out.println(zahl1 + " * " + zahl2 + " = " + ts.mul(zahl1, zahl2));
+				break;
+
+			case '4':
+				System.out.println(zahl1 + " / " + zahl2 + " = " + ts.div(zahl1, zahl2));
+				break;
+
+			default:
+				System.out.println("Invalid selection");
+				break; // This break is not really necessary
+			}
 		}
+		System.out.println("System beendet");
 
 	}
 

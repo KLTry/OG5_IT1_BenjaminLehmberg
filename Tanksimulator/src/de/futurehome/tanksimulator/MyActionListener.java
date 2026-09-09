f24b074f65be28b3f37df061a926f73d1d35a583
Benjamin <user@192.168.34.108>
2026-09-09 12:48:37 +0200

    Verbrauch wurde Immplementiert

--- Tanksimulator/src/de/futurehome/tanksimulator/MyActionListener.java
+++ Tanksimulator/src/de/futurehome/tanksimulator/MyActionListener.java
@@ -13,14 +13,27 @@ public class MyActionListener implements ActionListener {
 		Object obj = e.getSource();
 		if (obj == f.btnBeenden)
 			System.exit(0);
-		
+
 		if (obj == f.btnEinfuellen) {
-			 double fuellstand = f.myTank.getFuellstand();
-			 fuellstand = fuellstand + 5;
-			 f.myTank.setFuellstand(fuellstand);
+			double fuellstand = f.myTank.getFuellstand();
+			fuellstand = fuellstand + 5;
+			f.myTank.setFuellstand(fuellstand);
 
-			 f.lblFuellstand.setText(""+fuellstand);
+			f.lblFuellstand.setText(""+fuellstand); }
+
+			
+			if (obj == f.btnVerbrauchen && f.myTank.getFuellstand() > 1) {
+				 double verbrauch = f.myTank.getFuellstand();
+				 verbrauch = verbrauch - 2;
+				 f.myTank.setFuellstand(verbrauch);
+				
+				 f.lblFuellstand.setText(""+ verbrauch);
+				if(f.myTank.getFuellstand() <= 1) {
+					f.lblFuellstand.setText("Du musst Tanken");
+				
+				}
+			}
+
 		}
-
 	}
-}
+	

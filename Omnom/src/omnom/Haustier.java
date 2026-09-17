c731002236575c6d2b9b0b92ea97eedc86d1d5df
user <user@192.168.34.172>
2026-09-17 10:56:27 +0200

    Die Werte wurden mit 100 initialisiert im konstruktor

--- Omnom/src/omnom/Haustier.java
+++ Omnom/src/omnom/Haustier.java
@@ -12,8 +12,12 @@ public class Haustier {
 	//MEthoden
 	
 	public Haustier() {};
-	
-	public Haustier(String name) {}
+	public Haustier(String name) {
+		this.hunger = 100;
+		this.muede = 100; 
+		this.zufrieden = 100;
+		this.gesund = 100; 
+	}
 
 	public int getHunger() {
 		return hunger;

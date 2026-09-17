2c77759c4e9674d0084d2b734519d23fd568aba7
KLTry <user@192.168.34.172>
2026-09-17 10:28:36 +0200

    Attritbute und Methoden hinzugefügt. auf funktionalität geprüft

--- StarSim2099/src/de/oszimt/starsim2099/Pilot.java
+++ StarSim2099/src/de/oszimt/starsim2099/Pilot.java
@@ -3,13 +3,55 @@ package de.oszimt.starsim2099;
 /**
  * Write a description of class Pilot here.
  * 
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Benjamin Lehmberg)
+ * @version (17.09.2026)
  */
 public class Pilot {
 
-	// Attribute
-	
+	//Attribute
+	private String grad;
+	private String name;
+	private double posX;
+	private double posY;
+
 	// Methoden
+	public Pilot() {
+	}
+
+	public String getGrad() {
+		return grad;
+	}
+
+	public void setGrad(String grad) {
+		this.grad = grad;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPosX() {
+		return posX;
+	}
+
+	public void setPosX(double posX) {
+		this.posX = posX;
+	}
+
+	public double getPosY() {
+		return posY;
+	}
+
+	public void setPosY(double posY) {
+		this.posY = posY;
+	}
+
+
+
+
 
 }
--- StarSim2099/src/de/oszimt/starsim2099/Raumschiff.java
+++ StarSim2099/src/de/oszimt/starsim2099/Raumschiff.java
@@ -10,7 +10,64 @@ public class Raumschiff {
 
 	// Attribute
 	
+	private String typ;
+	private String antrieb;
+	private int maxLadekapazitaet;
+	private int winkel;
+	private double posX;
+	private double posY;
+	
 	// Methoden
+
+	public Raumschiff() {};
+	
+	public String getTyp() {
+		return typ;
+	}
+
+	public void setTyp(String typ) {
+		this.typ = typ;
+	}
+
+	public String getAntrieb() {
+		return antrieb;
+	}
+
+	public void setAntrieb(String antrieb) {
+		this.antrieb = antrieb;
+	}
+
+	public int getLadeKapazitaet() {
+		return maxLadekapazitaet;
+	}
+
+	public void setMaxLadekapazitaet(int ladeKapazitaet) {
+		this.maxLadekapazitaet = ladeKapazitaet;
+	}
+
+	public int getWinkel() {
+		return winkel;
+	}
+
+	public void setWinkel(int winkel) {
+		this.winkel = winkel;
+	}
+
+	public double getPosX() {
+		return posX;
+	}
+
+	public void setPosX(double posX) {
+		this.posX = posX;
+	}
+
+	public double getPosY() {
+		return posY;
+	}
+
+	public void setPosY(double posY) {
+		this.posY = posY;
+	}
 
 	// Darstellung
 	public static char[][] getDarstellung() {
